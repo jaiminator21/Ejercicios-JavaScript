ef5060211380a9b834c5408ac4f5fbcf5743ed3f
JimmyUpgrade <jaime.sebastian.erice@bootcamp-upgrade.com>
2024-01-20 10:27:59 +0100

    Cambio ejercicio 2 extra

--- S1/extra/exercise-2.js
+++ S1/extra/exercise-2.js
@@ -3,14 +3,11 @@
 const fruits = ['Strawberry', 'Banana', 'Orange', 'Apple'];
 let foodSchedule = [{ name: "Salad", isVegan: true }, { name: "Salmon", isVegan: false }, { name: "Tofu", isVegan: true }, { name: "Burger", isVegan: false }, { name: "Rice", isVegan: true }, { name: "Pasta", isVegan: true }];
 
-
 for (let i = 0; i < foodSchedule.length; i++) {
-    let foodSchedule2 = foodSchedule[i];
-    let foodSchedule3 = foodSchedule2.isVegan;
-    if (foodSchedule2.isVegan != true) {
-        let random = Math.floor(Math.random() * fruits.length);
-        foodSchedule2.name = fruits[random];
+    if (foodSchedule[i].isVegan != true) {
+        foodSchedule[i].name = fruits[i-1];
+        foodSchedule[i].isVegan = true
     }
-    console.log(foodSchedule2)
 }
+console.log(foodSchedule)
 
